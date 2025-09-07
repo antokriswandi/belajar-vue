a78d569d070a7684a046796346e942b352f62d69
AntSmile <antokriswandiyanto@gmail.com>
2025-09-07 21:50:40 +0700

    Episode 6 : Components props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,29 @@
 export default {
     template: `
-        <button class="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed" :disabled="isLoading">
-            <slot></slot>
+        <button
+            :class="{
+                'rounded px-4 py-2 text-white mx-1': true,
+                'bg-blue-500 hover:bg-blue-700': type === 'primary',
+                'bg-gray-500 hover:bg-gray-700': type === 'secondary',
+                'bg-green-500 hover:bg-green-700': type === 'success',
+                'bg-red-500 hover:bg-red-700': type === 'danger',
+                'bg-yellow-500 hover:bg-yellow-700': type === 'warning',
+                'opacity-50 cursor-not-allowed': isloading,
+            }"
+        >
+            <slot />
         </button>
-        `,
+    `,
 
-    data() {
-        return {
-            isLoading: true,
-        };
+    props: {
+        type: {
+            type: String,
+            default: 'primary', // primary, secondary, success, danger, warning
+        },
+
+        isloading: {
+            type: Boolean,
+            default: false,
+        },
     },
 };
